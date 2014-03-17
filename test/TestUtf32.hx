30ffa9cc32bcb948ee08fa33bd976e994d5876de
Ryusei Yamaguchi <mandel59@gmail.com>
2014-03-17 16:32:01 +0900

    modify Utf32 API

--- test/TestUtf32.hx
+++ test/TestUtf32.hx
@@ -6,64 +6,63 @@ import unifill.Utf32;
 class TestUtf32 extends haxe.unit.TestCase {
 
 	public function test_length() {
-		assertEquals(4, new Utf32("𩸽あëa").length);
+		assertEquals(4, Utf32.fromString("𩸽あëa").length);
 	}
 
 	public function test_concat() {
-		//assertEquals("𩸽あëa𩸽あëa", (new Utf32("𩸽あëa") + new Utf32("𩸽あëa")).toString());
-		var s = new Utf32("𩸽あëa");
+		var s = Utf32.fromString("𩸽あëa");
 		assertEquals("𩸽あëa𩸽あëa", (s + s).toString());
 	}
 
 	public function test_eq() {
-		assertTrue(new Utf32("𩸽あëa") == new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽あëaa") == new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽あëa") == new Utf32("𩸽あëaa"));
-		assertFalse(new Utf32("𩸽あëa") == new Utf32("𩸽あëb"));
-		assertFalse(new Utf32("𩸽あëb") == new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽") == new Utf32("�"));
+		assertTrue(Utf32.fromString("𩸽あëa") == Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽あëaa") == Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽あëa") == Utf32.fromString("𩸽あëaa"));
+		assertFalse(Utf32.fromString("𩸽あëa") == Utf32.fromString("𩸽あëb"));
+		assertFalse(Utf32.fromString("𩸽あëb") == Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽") == Utf32.fromString("�"));
 	}
 
 	public function test_lt() {
-		assertFalse(new Utf32("𩸽あëa") < new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽あëaa") < new Utf32("𩸽あëa"));
-		assertTrue(new Utf32("𩸽あëa") < new Utf32("𩸽あëaa"));
-		assertTrue(new Utf32("𩸽あëa") < new Utf32("𩸽あëb"));
-		assertFalse(new Utf32("𩸽あëb") < new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽") < new Utf32("�"));
+		assertFalse(Utf32.fromString("𩸽あëa") < Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽あëaa") < Utf32.fromString("𩸽あëa"));
+		assertTrue(Utf32.fromString("𩸽あëa") < Utf32.fromString("𩸽あëaa"));
+		assertTrue(Utf32.fromString("𩸽あëa") < Utf32.fromString("𩸽あëb"));
+		assertFalse(Utf32.fromString("𩸽あëb") < Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽") < Utf32.fromString("�"));
 	}
 
 	public function test_lte() {
-		assertTrue(new Utf32("𩸽あëa") <= new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽あëaa") <= new Utf32("𩸽あëa"));
-		assertTrue(new Utf32("𩸽あëa") <= new Utf32("𩸽あëaa"));
-		assertTrue(new Utf32("𩸽あëa") <= new Utf32("𩸽あëb"));
-		assertFalse(new Utf32("𩸽あëb") <= new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽") <= new Utf32("�"));
+		assertTrue(Utf32.fromString("𩸽あëa") <= Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽あëaa") <= Utf32.fromString("𩸽あëa"));
+		assertTrue(Utf32.fromString("𩸽あëa") <= Utf32.fromString("𩸽あëaa"));
+		assertTrue(Utf32.fromString("𩸽あëa") <= Utf32.fromString("𩸽あëb"));
+		assertFalse(Utf32.fromString("𩸽あëb") <= Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽") <= Utf32.fromString("�"));
 	}
 
 	public function test_gt() {
-		assertFalse(new Utf32("𩸽あëa") > new Utf32("𩸽あëa"));
-		assertTrue(new Utf32("𩸽あëaa") > new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽あëa") > new Utf32("𩸽あëaa"));
-		assertFalse(new Utf32("𩸽あëa") > new Utf32("𩸽あëb"));
-		assertTrue(new Utf32("𩸽あëb") > new Utf32("𩸽あëa"));
-		assertTrue(new Utf32("𩸽") > new Utf32("�"));
+		assertFalse(Utf32.fromString("𩸽あëa") > Utf32.fromString("𩸽あëa"));
+		assertTrue(Utf32.fromString("𩸽あëaa") > Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽あëa") > Utf32.fromString("𩸽あëaa"));
+		assertFalse(Utf32.fromString("𩸽あëa") > Utf32.fromString("𩸽あëb"));
+		assertTrue(Utf32.fromString("𩸽あëb") > Utf32.fromString("𩸽あëa"));
+		assertTrue(Utf32.fromString("𩸽") > Utf32.fromString("�"));
 	}
 
 	public function test_gte() {
-		assertTrue(new Utf32("𩸽あëa") >= new Utf32("𩸽あëa"));
-		assertTrue(new Utf32("𩸽あëaa") >= new Utf32("𩸽あëa"));
-		assertFalse(new Utf32("𩸽あëa") >= new Utf32("𩸽あëaa"));
-		assertFalse(new Utf32("𩸽あëa") >= new Utf32("𩸽あëb"));
-		assertTrue(new Utf32("𩸽あëb") >= new Utf32("𩸽あëa"));
-		assertTrue(new Utf32("𩸽") >= new Utf32("�"));
+		assertTrue(Utf32.fromString("𩸽あëa") >= Utf32.fromString("𩸽あëa"));
+		assertTrue(Utf32.fromString("𩸽あëaa") >= Utf32.fromString("𩸽あëa"));
+		assertFalse(Utf32.fromString("𩸽あëa") >= Utf32.fromString("𩸽あëaa"));
+		assertFalse(Utf32.fromString("𩸽あëa") >= Utf32.fromString("𩸽あëb"));
+		assertTrue(Utf32.fromString("𩸽あëb") >= Utf32.fromString("𩸽あëa"));
+		assertTrue(Utf32.fromString("𩸽") >= Utf32.fromString("�"));
 	}
 
 	public function test_cons_and_snoc() {
 		var c = new CodePoint(0x29E3D);
-		assertTrue(new Utf32("𩸽あëa") == c + new Utf32("あëa"));
-		assertTrue(new Utf32("あëa𩸽") == new Utf32("あëa") + c);
+		assertTrue(Utf32.fromString("𩸽あëa") == c + Utf32.fromString("あëa"));
+		assertTrue(Utf32.fromString("あëa𩸽") == Utf32.fromString("あëa") + c);
 	}
 
 }
--- unifill/Utf32.hx
+++ unifill/Utf32.hx
@@ -3,57 +3,61 @@ package unifill;
 /**
    Utf32 provides a UTF-32-encoded string.
  **/
-abstract Utf32(Array<CodePoint>) {
+abstract Utf32(Array<Int>) {
+
+	public static inline function fromCharCode(code : Int) : Utf32 {
+		return new Utf32([code]);
+	}
+
+	public static inline function fromString(string : String) : Utf32 {
+		var u = [for (c in new InternalEncodingIter(string, 0, string.length)) InternalEncoding.codePointAt(string, c)];
+		return new Utf32(u);
+	}
+
+	public static inline function fromArray(a : Array<Int>) : Utf32 {
+		return new Utf32(a.copy());
+	}
 
 	public var length(get, never) : Int;
 
-	inline function get_length() : Int {
-		return this.length;
-	}
-
-	public inline function new(string : String) : Void {
-		InternalEncoding.validate(string);
-		this = [for (c in new InternalEncodingIter(string, 0, string.length)) cast InternalEncoding.codePointAt(string, c)];
-	}
-
-	public inline function toUpperCase() : Utf32 {
-		return cast [for (c in this) if ("a".code <= c.toInt() && c.toInt() <= "z".code) c - ("a".code - "A".code) else c];
-	}
-
-	public inline function toLowerCase() : Utf32 {
-		return cast [for (c in this) if ("A".code <= c.toInt() && c.toInt() <= "Z".code) c - ("A".code - "a".code) else c];
-	}
-
 	public inline function charAt(index : Int) : Utf32 {
 		if (0 <= index && index < this.length) {
-			return cast [this[index]];
+			return new Utf32([this[index]]);
 		} else {
-			return cast [];
+			return new Utf32([]);
 		}
 	}
 
 	public inline function charCodeAt(index : Int) : Null<Int> {
 		if (0 <= index && index < this.length) {
-			return this[index].toInt();
+			return this[index];
 		}
 		return null;
 	}
 
-	public inline function toString() : String {
-		return InternalEncoding.fromCodePoints(cast this);
+	public inline function toUpperCase() : Utf32 {
+		return new Utf32([for (c in this) if ("a".code <= c && c <= "z".code) c - ("a".code - "A".code) else c]);
 	}
 
-	public static inline function fromCharCode(code : Int) : Utf32 {
-		return cast [new CodePoint(code)];
+	public inline function toLowerCase() : Utf32 {
+		return new Utf32([for (c in this) if ("A".code <= c && c <= "Z".code) c - ("A".code - "a".code) else c]);
+	}
+
+	public inline function toString() : String {
+		return InternalEncoding.fromCodePoints(this);
+	}
+
+	public inline function toArray() : Array<Int> {
+		return this.copy();
 	}
 
 	@:op(A + B)
-	public static inline function concat(a : Utf32, b : Utf32) : Utf32 {
-		var s : Array<CodePoint> = cast a;
-		return cast s.concat(cast b);
+	static inline function concat(a : Utf32, b : Utf32) : Utf32 {
+		var s : Array<Int> = cast a;
+		return new Utf32(s.concat(cast b));
 	}
 
-	static function cmp(a : Array<CodePoint>, b : Array<CodePoint>) : Int {
+	static function cmp(a : Array<Int>, b : Array<Int>) : Int {
 		var alen = a.length;
 		var blen = b.length;
 		var len = if (alen < blen) blen else alen;
@@ -73,50 +77,58 @@ abstract Utf32(Array<CodePoint>) {
 	}
 
 	@:op(A == B)
-	public static inline function eq(a : Utf32, b : Utf32) : Bool {
+	static inline function eq(a : Utf32, b : Utf32) : Bool {
 		return if (a.length != b.length) false;
 		else cmp(cast a, cast b) == 0;
 	}
 
 	@:op(A != B)
-	public static inline function ne(a : Utf32, b : Utf32) : Bool {
+	static inline function ne(a : Utf32, b : Utf32) : Bool {
 		return !eq(a, b);
 	}
 
 	@:op(A < B)
-	public static inline function lt(a : Utf32, b : Utf32) : Bool {
+	static inline function lt(a : Utf32, b : Utf32) : Bool {
 		return cmp(cast a, cast b) < 0;
 	}
 
 	@:op(A <= B)
-	public static inline function lte(a : Utf32, b : Utf32) : Bool {
+	static inline function lte(a : Utf32, b : Utf32) : Bool {
 		return cmp(cast a, cast b) <= 0;
 	}
 
 	@:op(A > B)
-	public static inline function gt(a : Utf32, b : Utf32) : Bool {
+	static inline function gt(a : Utf32, b : Utf32) : Bool {
 		return cmp(cast a, cast b) > 0;
 	}
 
 	@:op(A >= B)
-	public static inline function gte(a : Utf32, b : Utf32) : Bool {
+	static inline function gte(a : Utf32, b : Utf32) : Bool {
 		return cmp(cast a, cast b) >= 0;
 	}
 
 	@:op(A + B)
-	public static inline function cons(a : CodePoint, b : Utf32) : Utf32 {
-		var c : Array<CodePoint> = cast b;
+	static inline function cons(a : CodePoint, b : Utf32) : Utf32 {
+		var c : Array<Int> = cast b;
 		var d = c.copy();
-		d.unshift(a);
+		d.unshift(a.toInt());
 		return cast d;
 	}
 
 	@:op(A + B)
-	public static inline function snoc(a : Utf32, b : CodePoint) : Utf32 {
-		var c : Array<CodePoint> = cast a;
+	static inline function snoc(a : Utf32, b : CodePoint) : Utf32 {
+		var c : Array<Int> = cast a;
 		var d = c.copy();
-		d.push(b);
+		d.push(b.toInt());
 		return cast d;
+	}
+
+	inline function get_length() : Int {
+		return this.length;
+	}
+
+	inline function new(a : Array<Int>) {
+		this = a;
 	}
 
 }
