c9000719610a9a87fdf24aefd722db29aa5ce7d9
Ryusei Yamaguchi <mandel59@gmail.com>
2015-06-20 17:00:24 +0900

    modify to use inline constructor optimization effectively

--- unifill/CodePointIter.hx
+++ unifill/CodePointIter.hx
@@ -1,19 +1,26 @@
 package unifill;
 
 class CodePointIter {
-	var s : String;
-	var itr : InternalEncodingIter;
+
+	public var string : String;
+	public var index : Int;
+	public var endIndex : Int;
 
 	public inline function new(s : String) {
-		this.s = s;
-		this.itr = new InternalEncodingIter(s, 0, s.length);
+		string = s;
+		index = 0;
+		endIndex = s.length;
 	}
 
 	public inline function hasNext() : Bool {
-		return itr.hasNext();
+		return index < endIndex;
 	}
 
+	var i = 0; // FIXME: blocked by HaxeFoundation/haxe#4353
 	public inline function next() : CodePoint {
-		return cast InternalEncoding.codePointAt(s, itr.next());
+		i = index;
+		index += InternalEncoding.codePointWidthAt(string, index);
+		return cast InternalEncoding.codePointAt(string, i);
 	}
+
 }
