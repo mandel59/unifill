c3bc6b89467299a1ce358dc755951457fad32e9d
Ryusei Yamaguchi <mandel59@gmail.com>
2020-07-22 02:55:03 +0900

    Enable all targets except swf

--- run-test-on-docker.sh
+++ run-test-on-docker.sh
@@ -2,4 +2,4 @@ dir="$(dirname "$(readlink -f "$0")")"
 
 docker build -t mandel59/unifill-test-runner -f unifill-test-runner.dockerfile .
 mkdir -p "$dir/test"
-docker run --volume "$dir/test:/opt/unifill/test" mandel59/unifill-test-runner haxe compile-test.hxml
+docker run --volume "$dir/test:/opt/unifill/test" mandel59/unifill-test-runner lix compile-test.hxml
